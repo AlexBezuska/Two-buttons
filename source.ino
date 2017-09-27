f935da7a3eb9134447711331694f2b150f4efe12
Alex Bezuska <abezuska@gmail.com>
2017-09-26 22:27:23 -0400

    Update source.ino

--- source.ino
+++ source.ino
@@ -13,9 +13,9 @@ void loop() {
 
    // LEFT
   int newPin10 = digitalRead(10);
-  if (newPin10 == 1 && lastPin10 == 0) {
+  if (newPin10 == 0 && lastPin10 == 1) {
     Keyboard.press(0xd7);
-  } else if (newPin10 == 0 && lastPin10 == 1) {
+  } else if (newPin10 == 1 && lastPin10 == 0) {
    Keyboard.release(0xd7);
   }
   lastPin10 = newPin10;
@@ -23,9 +23,9 @@ void loop() {
 
   // RIGHT
   int newPin9 = digitalRead(9);
-  if (newPin9 == 1 && lastPin9 == 0) {
+  if (newPin9 == 0 && lastPin9 == 1) {
     Keyboard.press(0xd8);
-  } else if (newPin9 == 0 && lastPin9 == 1) {
+  } else if (newPin9 == 1 && lastPin9 == 0) {
    Keyboard.release(0xd8);
   }
   lastPin9 = newPin9;
