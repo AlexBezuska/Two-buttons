b563d35afe419315acff15ff3ddd6e36d8b3aa40
Alex Bezuska <abezuska@gmail.com>
2017-09-26 20:13:30 -0400

    Create source.ino

---
+++ source.ino
@@ -0,0 +1,32 @@
+#include <Keyboard.h>
+
+  int lastPin10 = 0;
+  int lastPin9 = 0;
+
+void setup() {
+
+}
+
+void loop() {
+
+   // LEFT
+  int newPin10 = digitalRead(10);
+  if (newPin10 == 1 && lastPin10 == 0) {
+    Keyboard.press(0xd7);
+  } else if (newPin10 == 0 && lastPin10 == 1) {
+   Keyboard.release(0xd7);
+  }
+  lastPin10 = newPin10;
+ 
+
+  // RIGHT
+  int newPin9 = digitalRead(9);
+  if (newPin9 == 1 && lastPin9 == 0) {
+    Keyboard.press(0xd8);
+  } else if (newPin9 == 0 && lastPin9 == 1) {
+   Keyboard.release(0xd8);
+  }
+  lastPin9 = newPin9;
+
+  
+}
